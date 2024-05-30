0a63d83e51e7c6f31562d6cb9e829cf9e3c96681
Neeraj Verma <neeraj708055@gmail.com>
2024-05-30 18:43:44 +0530

    Prodios Labs

---
+++ simple/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./App.css";
+import CodeEditor from './CodeEditor';
+function App() {
+  return (
+    <div className="App">
+      <div className="Header">Simple Code Editor</div>
+      <CodeEditor />
+    </div>
+  );
+}
+
+export default App;
---
+++ simple/src/CodeEditor.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect, useRef } from "react";
+
+import Prism from "prismjs";  // Prism.js library for syntax highlighting 
+import "./App.css";
+import "prismjs/themes/prism.css";  //Prism.js theme CSS
+
+const CodeEditor = () => {
+  const [code, setCode] = useState("");
+  const codeRef = useRef(null); // useRef for referencing the code element
+
+  useEffect(() => {
+    Prism.highlightElement(codeRef.current); // HighlightElement use to highlight current element when it changes
+  }, [code]);// Trigger useeffect when code changes
+
+
+  return (
+    <div className="code-editor">
+
+      <textarea
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+        placeholder="Type Something Here....."
+      />
+
+      {/* by using className="language-js" informs Prism.js to highlight the code as JavaScript */}
+      {/* use <pre>  because it preserves whitespace, maintains consistent alignment with a monospace font , better to use with prism */}
+      <pre className="language-js">
+        {/* Use useRef for creating a persistent reference that doesn't trigger re-renders. It is handy for managing DOM elements and storing values. */}
+        <code ref={codeRef} className="language-js">
+          {code} {/* Display the code entered by the user */}
+        </code>
+      </pre>
+    </div>
+  );
+};
+
+export default CodeEditor;
